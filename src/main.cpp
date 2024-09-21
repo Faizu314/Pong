2c9b9f7e388e02f1ef78c8eebd3d60d10c825589
Faizan Qaiser <faizanqaiser9@gmail.com>
2024-09-21 18:07:19 +0500

    Added correct windows dlls and libs
    Specifically not ignoring lib files in lib/windows
    Configured cmake to build a GUI windows application on windows platform
    Conditionally defining the correct main signature depending on the platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include "application.hpp"
 
-int main(int argc, const char * argv[]) {
+#ifdef _WIN32
+
+int WinMain(int argc, const char * argv[]) {
     RunApp();
 
     return 0;
 }
+
+#elif _APPLE_
+
+int main(int argc, const char* argv[]) {
+    RunApp();
+}
+
+#endif
